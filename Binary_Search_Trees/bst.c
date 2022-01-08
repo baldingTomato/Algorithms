623f0e71fc7972ed4aa5d275457e650f57d8ba81
baldingTomato <86657058+baldingTomato@users.noreply.github.com>
2022-01-08 21:57:33 +0100

    Added function for printing tree

--- Binary_Search_Trees/bst.c
+++ Binary_Search_Trees/bst.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct Node {
 
@@ -166,23 +167,80 @@ void deleteNode(Node *parent, int val){
 
 }
 
+void padding(char ch, int n){
+
+    for (int i = 0; i < n; i++){
+        putchar(ch);
+    }
+
+}
+
+void printTree(Node *root, int level){
+  
+  if(root == NULL){
+    padding('\t', level);
+    puts ("~");
+  }else{
+    printTree(root->right_child, level + 1);
+    padding('\t', level);
+    printf("%d\n", root->value);
+    printTree(root->left_child, level + 1);
+  }
+
+}
+
 int main(){
 
     Node *root1 = createNode(7);
-    
+
     addNode(root1, 3);
     addNode(root1, 5);
     addNode(root1, 10);
     addNode(root1, 1);
     addNode(root1, 9);
     addNode(root1, 8);
-    
+    addNode(root1, 12);
+    addNode(root1, 20);
+    addNode(root1, 22);
+    addNode(root1, 2);
+    addNode(root1, 100);
+    addNode(root1, 4);
+    addNode(root1, 17);
+    addNode(root1, 16);
+    addNode(root1, 13);
+
     searchNode(root1, 3);
     searchNode(root1, 10);
     searchNode(root1, 5);
     searchNode(root1, 9);
     searchNode(root1, 1);
     searchNode(root1, 7);
-    
+/*
+    deleteNode(root1, 9);
+    deleteNode(root1, 10);
+    deleteNode(root1, 3);
+    deleteNode(root1, 3);
+    deleteNode(root1, 7);
+
+    searchNode(root1, 3);
+    searchNode(root1, 10);
+    searchNode(root1, 5);
+    searchNode(root1, 9);
+    searchNode(root1, 1);
+    searchNode(root1, 7);
+*/
+
+    printf("---------------------\n\n\n\n");
+
+    printTree(root1, 1);
+
+    deleteNode(root1, 9);
+    deleteNode(root1, 1);
+    deleteNode(root1, 10);
+
+    printf("---------------------\n\n\n\n");
+
+    printTree(root1, 1);
+
     return 0;
 }
