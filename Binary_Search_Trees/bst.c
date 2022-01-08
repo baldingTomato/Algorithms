f898930e8cd09e76000e9078adb147d8d37ae095
baldingTomato <86657058+baldingTomato@users.noreply.github.com>
2022-01-08 21:54:26 +0100

    Added functions for adding nodes

--- Binary_Search_Trees/bst.c
+++ Binary_Search_Trees/bst.c
@@ -29,3 +29,55 @@ Node *createNode(int val){
 
     return node;
 }
+
+void addNode(Node *parent, int val){
+
+    bool inserted = false;
+
+    while(inserted == false){
+
+        if(val < parent->value){
+
+            if(parent->left_child == NULL){
+                parent->left_child = createNode(val);
+                inserted = true;
+            }
+            parent = parent->left_child;
+
+        }else if(val > parent->value){
+
+            if(parent->right_child == NULL){
+                parent->right_child = createNode(val);
+                inserted = true;
+            }
+            parent = parent->right_child;
+
+        }else if(val == parent->value){
+            parent->repetition++;
+            inserted = true;
+        }
+
+    }
+    
+}
+
+int main(){
+
+    Node *root1 = createNode(7);
+    
+    addNode(root1, 3);
+    addNode(root1, 5);
+    addNode(root1, 10);
+    addNode(root1, 1);
+    addNode(root1, 9);
+    addNode(root1, 8);
+    
+    searchNode(root1, 3);
+    searchNode(root1, 10);
+    searchNode(root1, 5);
+    searchNode(root1, 9);
+    searchNode(root1, 1);
+    searchNode(root1, 7);
+    
+    return 0;
+}
