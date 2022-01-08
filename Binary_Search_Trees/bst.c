22fc65c054bfc70746c793ef6ea859855b5ff599
baldingTomato <86657058+baldingTomato@users.noreply.github.com>
2022-01-08 21:56:02 +0100

    Added functions for searching and deleting nodes

--- Binary_Search_Trees/bst.c
+++ Binary_Search_Trees/bst.c
@@ -61,6 +61,111 @@ void addNode(Node *parent, int val){
     
 }
 
+void searchNode(Node *parent, int val){
+
+    bool found = false;
+
+    while(found == false){
+
+        if(val < parent->value){
+
+            if(parent->left_child != NULL){
+                parent = parent->left_child;
+            }else{
+                printf("The tree has no such value like %d!\n", val);
+                return;
+            }
+
+
+        }else if(val > parent->value){
+
+            if(parent->right_child != NULL){
+                parent = parent->right_child;
+            }else{
+                printf("The tree has no such value like %d!\n", val);
+                return;
+            }
+
+        }else if(val == parent->value){
+            found = true;
+            printf("Found value %d!\n", parent->value);
+        }
+
+    }
+
+}
+
+Node *minValue(Node *parent){
+
+    while(0 == 0){
+
+        if(parent->left_child == NULL){
+            return parent;
+        }
+
+        parent = parent->left_child;
+
+    }
+
+    return parent;
+}
+
+void deleteNode(Node *parent, int val){
+
+    bool deleted = false;
+
+    while(deleted == false){
+
+        if(val < parent->value){
+
+            if(parent->left_child == NULL){
+                printf("The tree has no such value like %d!\n", val);
+                return;
+            }else{
+                parent = parent->left_child;
+            }
+
+
+        }else if(val > parent->value){
+
+            if(parent->right_child == NULL){
+                printf("The tree has no such value like %d!\n", val);
+                return;
+            }else{
+                parent = parent->right_child;
+            }
+
+        }else if(val == parent->value){
+            
+            if(parent->left_child == NULL && parent->right_child == NULL){
+                parent = NULL;
+                deleted = true;
+            }else if(parent->left_child != NULL && parent->right_child == NULL){
+                parent->value = parent->left_child->value;
+                parent->left_child = NULL;
+                deleted = true;
+            }else if(parent->left_child == NULL && parent->right_child != NULL){
+                parent->value = parent->right_child->value;
+                parent->right_child = NULL;
+                deleted = true;
+            }else if(parent->left_child != NULL && parent->right_child != NULL){
+                Node *successor = createNode(0);
+                successor = minValue(parent->right_child);
+                printf("%d", successor->value);
+                parent->value = successor->value;
+                deleteNode(parent->right_child, successor->value);
+                successor = NULL;
+                deleted = true;
+            }
+
+        }
+
+    }
+
+    printf("Value %d was successfully deleted!\n", val);
+
+}
+
 int main(){
 
     Node *root1 = createNode(7);
